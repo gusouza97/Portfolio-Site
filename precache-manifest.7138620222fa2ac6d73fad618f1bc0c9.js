bbda39fa70ca39a0960f5890d7647a0f024d3837
Gustavo Souza <36318691+gusouza97@users.noreply.github.com>
2022-03-29 19:52:56 -0300

    msg

---
+++ precache-manifest.7138620222fa2ac6d73fad618f1bc0c9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e34a353b422df3e9958a",
+    "url": "./static/css/main.189fbece.chunk.css"
+  },
+  {
+    "revision": "e34a353b422df3e9958a",
+    "url": "./static/js/main.e34a353b.chunk.js"
+  },
+  {
+    "revision": "2d3efeb71c7eebfe7cd0",
+    "url": "./static/js/1.2d3efeb7.chunk.js"
+  },
+  {
+    "revision": "90b70046d89165803543",
+    "url": "./static/css/2.1358d7c2.chunk.css"
+  },
+  {
+    "revision": "90b70046d89165803543",
+    "url": "./static/js/2.90b70046.chunk.js"
+  },
+  {
+    "revision": "fdb8a5740a09d87d11e1",
+    "url": "./static/js/runtime~main.fdb8a574.js"
+  },
+  {
+    "revision": "6536537d99d74f46f6565d4b05422b4f",
+    "url": "./static/media/project001.6536537d.PNG"
+  },
+  {
+    "revision": "3b761cd07489572d43269933b02ae4ff",
+    "url": "./static/media/about-img.3b761cd0.jpg"
+  },
+  {
+    "revision": "adb33906befda4ec3e526ae48c2b632d",
+    "url": "./static/media/project002.adb33906.PNG"
+  },
+  {
+    "revision": "fbb6ca195b4815c88ced033aec9d84d2",
+    "url": "./index.html"
+  }
+];
